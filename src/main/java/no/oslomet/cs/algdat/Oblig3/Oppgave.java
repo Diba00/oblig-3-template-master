66d6e2e409df54cc9877507a221b39610b4688b1
Diba00 <dibas.shishegar@gmail.com>
2022-10-24 15:00:45 +0200

    la til T og v og h som null

--- src/main/java/no/oslomet/cs/algdat/Oblig3/Oppgave.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/Oppgave.java
@@ -6,28 +6,4 @@ public interface Oppgave<T>  // legges under hjelpeklasser
     void utførOppgave(T t);    // f.eks. utskrift til konsollet
 }
 
-    public boolean leggInn(T verdi){
-        Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
-
-        Node<T> p = rot, q = null;               // p starter i roten
-        int cmp = 0;                             // hjelpevariabel
-
-        while (p != null)       // fortsetter til p er ute av treet
-        {
-            q = p;                                 // q er forelder til p
-            cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
-            p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
-        }
-
-        // p er nå null, dvs. ute av treet, q er den siste vi passerte
-
-        p = new Node<>(verdi);                   // oppretter en ny node
-
-        if (q == null) rot = p;                  // p blir rotnode
-        else if (cmp < 0) q.venstre = p;         // venstre barn til q
-        else q.høyre = p;                        // høyre barn til q
-
-        antall++;                                // én verdi mer i treet
-        return true;                             // vellykket innlegging
-    }
 
